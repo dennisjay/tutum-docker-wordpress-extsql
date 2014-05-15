1c54b54af7f9bd082088ce5b663608472c4ab0ae
Dennis Jöst <djprivat@googlemail.com>
2014-05-15 18:54:51 +0200

    script

--- create-connection.py
+++ create-connection.py
@@ -1,8 +1,20 @@
 import boto.ec2
+import yaml 
+
 conn=boto.ec2.connect_to_region("eu-west-1")
 reservations = conn.get_all_instances()
 for res in reservations:
     for inst in res.instances:
         if 'type' in inst.tags:
             if inst.tags['type'] == 'db':
-                print inst.ip_address()
+                print inst.ip_address
+                
+                with open("fig.yml") as f:
+                    dataMap = yaml.safe_load(f)
+                    print(dataMap)
+                    
+                dataMap['wordpress']['environment']['WORDPRESS_DB_IP'] = str(inst.ip_address)   
+                
+                with open("fig.yml", "w") as f:
+                    yaml.dump(dataMap, f)
+                    
